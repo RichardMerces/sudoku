6162370e49c5f0a0baf44e1171e0969094ddc764
Richard Merces <richardmerces@outlook.com>
2025-07-08 19:40:23 -0300

    Desafio sudoko

---
+++ src/br/com/dio/model/Board.java
@@ -0,0 +1,68 @@
+package br.com.dio.model;
+
+import static br.com.dio.model.GameStatusEnum.COMPLETE;
+import static br.com.dio.model.GameStatusEnum.INCOMPLETE;
+import static br.com.dio.model.GameStatusEnum.NON_STARTED;
+import java.util.Collection;
+import java.util.List;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+public class Board {
+
+    private final List<List<Space>> spaces;
+
+    public Board(List<List<Space>> spaces) {
+        this.spaces = spaces;
+    }
+
+    public List<List<Space>> getSpaces() {
+        return spaces;
+    }
+
+    public GameStatusEnum getStatusEnum() {
+        if(spaces.stream().flatMap(Collection::stream).noneMatch(s -> !s.isFixed() && nonNull(s.getActual()))) {
+            return  NON_STARTED;
+        }
+
+        return spaces.stream().flatMap(Collection::stream).anyMatch(s -> isNull(s.getActual())) ? INCOMPLETE : COMPLETE;
+    }
+
+    public boolean hasErrors() {
+        if(getStatusEnum() == NON_STARTED) {
+            return false;
+        }
+
+        return spaces.stream().flatMap(Collection::stream).anyMatch(s -> isNull(s.getActual()) && !s.getActual().equals(s.getExpected()));
+    }
+
+    public boolean changeValue(final int col, final int row, final Integer value) {
+        var space = spaces.get(col).get(row);
+
+        if (space.isFixed()) {
+            return false;
+        }
+
+        space.setActual(value);
+        return true;
+    }
+
+    public boolean clearValue(final int col, final int row) {
+        var space = spaces.get(col).get(row);
+
+        if (space.isFixed()) {
+            return false;
+        }
+
+        space.clearSpace();
+        return true;
+    }
+
+    public void reset() {
+        spaces.forEach(c -> c.forEach(Space::clearSpace));
+    }
+    
+    public boolean gameIsFinished() {
+        return !hasErrors() && getStatusEnum().equals(COMPLETE);
+    }
+}
